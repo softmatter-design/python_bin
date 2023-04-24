7ebc46ca48c0f054b707e6f8ed1df99266cb32a7
Softmatters <58212320+softmatter-design@users.noreply.github.com>
2023-04-24 17:38:14 +0900

    0424

---
+++ bin/make_md.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+##### Import #############################################
+import glob
+import os
+import shutil
+###############
+# File Select
+def file_listing():
+	target_list = ['**/all.dat', '**/ave.dat', '**/averaged.png', '**/plot_all.plt', '**/series.png']
+	for target in target_list:
+		src_list = glob.glob(target, recursive=True)
+		for src in src_list:
+			dst = os.path.join('./data', src)
+			dirname = os.path.dirname(dst)
+			os.makedirs(dirname, exist_ok=True)
+			shutil.copy(src, dst)
+	return 
+
+##########################
+# Main
+if __name__=='__main__':
+	file_listing()
