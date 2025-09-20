bea52114c8444c49903826eb255d85f07234520a
Softmatters <58212320+softmatter-design@users.noreply.github.com>
2025-09-20 18:49:06 +0900

    古いバージョンを削除

--- scripts/old/modules_1/__init__.py
+++
@@ -1,8 +0,0 @@
-import modules_1.ReadNWConditions
-import modules_1.SetupInitUDF
-import modules_1.NWSetup
-# import modules_1.RegNWSetup
-# import modules_1.RndNWSetup
-import modules_1.EquivCalcSetup
-
-__version__ = '0.1'
